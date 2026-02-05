fe1e4e377d356685a9c31fef642bee1cd53b7c41
BekassX <100470095+BekassX@users.noreply.github.com>
2026-02-06 01:30:22 +0330

    Add files via upload

---
+++ unis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sleep 3
+
+cd /root/
+
+echo "Unistalling ..."
+
+rm d.sh
+
+rm g.sh
+
+rm e.sh
+
+rm c.sh
+
+rm /etc/init.d/a
+
+rm /sbin/a
+
+cd /root/
+
+echo "Done !"
+
+sleep 5
+
+rm unis.sh
